9821e7d3731258a4cc69922ae20fb355ecee5726
scarlettfres <scarlett_fres@hotmail.com>
2014-12-14 11:56:39 +0100

    OK time changement
    code javascript pour MAJ le temps et l'afficher OK

--- war/js/ajout_plan.js
+++ war/js/ajout_plan.js
@@ -1,40 +1,47 @@
 var i=0;
+oldDate = new Date(0,0,0,0,0,0);
 function test()
 {
-	
-	
-      
+	  
    // console.log(fruits.length);
-	var tableauTitle ;  // Public 
-	var tableauDuree ; 
+	var tableauTitle ;  
 	var tableauDescr ; 
 	var heure= parseInt($("#heure").val());
 	var minute =parseInt ($("#minute").val());
 	var seconde=parseInt($("#seconde").val());
-	
-	
-	/*if(heure!= NaN && minute!=NaN && seconde!= NaN)	// retourne vrai si non un entier 
-		{
-			
-			alert("ouaaai");
-		}*/
-
 	tableauTitle= $("#titleDescription").val(); 
 	tableauDescr=  $("#exerciceDescription").val();
-	//isNaN(valeur); //retourne vrai si valeur  
-	var temps = $("#heure").val() + $("#minute").val() + $("#seconde").val();
-	tableauDuree = temps;
-	i=i+1;
 	
-	 $('#exercicesAdded').append("<tr id="+i+"><td> </td><td>"+ tableauTitle+"</td> <td class='hidden-xs'><p>"+
-			 tableauDescr+
-			 "</p></td><td>"+
-			 tableauDuree+
-			 '</td> <td><button onClick="supr(\''+i+'\');" type="submit" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-remove"></span> </button></td></tr>' 
-	 );
+	
+	//document.write(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + 
 
-
-
+	
+	if (isNaN(heure)|| isNaN(minute) || isNaN(seconde) ){alert(" Le format de la durée n'est pas un chiffre ! ");}
+	else if (tableauTitle==""){ alert("veuillez entrer un titre ")}
+	else if (tableauDescr==""){ alert("veuillez entrer une description ")}
+	else 
+	{	
+		date1 = new Date(0,0,0,heure,minute,seconde);
+		//oldDate=oldDate+date1;
+		
+		oldDate.setSeconds(oldDate.getSeconds() + seconde);
+		oldDate.setMinutes(oldDate.getMinutes() + minute);
+		oldDate.setHours(oldDate.getHours() + heure);
+		
+		var oldtemps=Math.round((oldDate.getHours()*60+oldDate.getMinutes()+oldDate.getSeconds()/60)*100)/100;
+		var temps = Math.round((heure*60+minute+seconde/60)*100)/100;
+		i=i+1;
+		
+		
+		 $('#timeID').text(oldtemps+" min"); 
+		 
+		 $('#exercicesAdded').append("<tr id="+i+"><td> </td><td>"+ tableauTitle+"</td> <td class='hidden-xs'><p>"+
+				 tableauDescr+
+				 "</p></td><td>"+
+				 temps+ " min"+
+				 '</td> <td><button onClick="supr(\''+i+'\');" type="submit" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-remove"></span> </button></td></tr>' 
+		 );
+	}
 }
 
 function supr(id)
