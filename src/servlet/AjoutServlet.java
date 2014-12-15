7ec6a3a1b6978ddba6a3a3856b85ff374cade714
scarlettfres <scarlett_fres@hotmail.com>
2014-12-15 11:26:19 +0100

    Halleluia
    Il manquait un fichier " test" dans les src, ajout servlet OK

---
+++ src/servlet/AjoutServlet.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
+public class AjoutServlet extends HttpServlet {
+	private DatastoreService datastore;
+       
+    public AjoutServlet() {
+        super();
+    }
+    
+   @Override
+   public void init() throws ServletException {
+	   super.init();
+	   //Create a connection to the datastore ONETIME at the init servlet process
+		datastore = DatastoreServiceFactory.getDatastoreService();
+   }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Entity text = new Entity("text");
+		text.setProperty("Title","welcome");
+		text.setProperty("descript", "ceci est un message daccueil ");
+		datastore.put(text);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+		}
+	
+	}
