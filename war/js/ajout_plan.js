dadf6e69555f4e15c2ecb7df4e175720499943fb
scarlettfres <scarlett_fres@hotmail.com>
2014-12-18 19:59:00 +0100

    Revert "train"
    This reverts commit 0b0a327e50d0cc6da131e0b9e4bdf33347377b57.

--- war/js/ajout_plan.js
+++ war/js/ajout_plan.js
@@ -1,8 +1,5 @@
 var i=0;
 var tab_duree = [];
-var tableauTitle = [];
-var tableauDescr = [];
-
 function MAJtempsTot()
 {
 	oldDate = new Date(0,0,0,0,0,0);
@@ -18,30 +15,27 @@ function MAJtempsTot()
 
 function test()
 {
-	
+	var tableauTitle ;  
+	var tableauDescr ; 
 	var heure= parseInt($("#heure").val());
 	var minute =parseInt ($("#minute").val());
 	var seconde=parseInt($("#seconde").val());
-	titre= $("#titleDescription").val(); 
-	descr=  $("#exerciceDescription").val();
+	tableauTitle= $("#titleDescription").val(); 
+	tableauDescr=  $("#exerciceDescription").val();
 	
 	if (isNaN(heure)|| isNaN(minute) || isNaN(seconde) ){alert(" Le format de la durée n'est pas un chiffre ! ");}
-	else if (titre==""){ alert("veuillez entrer un titre ")}
-	else if (descr==""){ alert("veuillez entrer une description ")}
+	else if (tableauTitle==""){ alert("veuillez entrer un titre ")}
+	else if (tableauDescr==""){ alert("veuillez entrer une description ")}
 	else 
-	{	
-		tableauTitle.push(titre); 
-		tableauDescr.push(descr);
-		
-		// --------------Ajout des temps --------------
+	{	// --------------Ajout des temps --------------
 		date1=new Date(0,0,0,heure,minute,seconde);
 		tab_duree.push(date1);
 		MAJtempsTot();
 		var temps = Math.round((heure*60+minute+seconde/60)*100)/100;	
 		// --------------/ Ajout des temps --------------
 		i=i+1;
-		 $('#exercicesAdded').append("<tr id="+i+"><td> </td><td>"+ tableauTitle[i -1]+"</td> <td class='hidden-xs'><p>"+
-				 tableauDescr[i-1]+
+		 $('#exercicesAdded').append("<tr id="+i+"><td> </td><td>"+ tableauTitle+"</td> <td class='hidden-xs'><p>"+
+				 tableauDescr+
 				 "</p></td><td>"+
 				 temps+ " min"+
 				 '</td> <td><button onClick="supr(\''+i+'\');" type="submit" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-remove"></span> </button></td></tr>' 
@@ -65,19 +59,15 @@ function datastore()
 	var inputDescription= $("#inputDescription").val();
 	var domain= $("#e1").val();
 	var timeID= $("#timeID").text();
-	/*var exercicesTitle= $("#TableauTitle").text();
-	var exercicesDescr= $("#TableauDescr").text();
-	var exercicesTime= $("#Tab_duree").text();*/
+	var exercicesAdded= $("#exercicesAdded").text();
 	
 	$.get('ajout', 
 	{ 
+		'exercicesAdded':exercicesAdded,
 		'inputTitle':inputTitle,
 		'inputDescription':inputDescription,
 		'domain':domain,
-		'timeID':timeID,
-		'exercicesTabTitle':tableauTitle,
-		'exercicesTabDescr':tableauDescr,
-		'exercicesTabTime':tab_duree	
+		'timeID':timeID
 	},
 	function(responseText)
 	{
