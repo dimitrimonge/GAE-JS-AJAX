7c1221866dcb49c572d6184c1f321c4913ddce83
dimitritrimonge <mongedimitri@gmail.com>
2014-12-19 17:43:40 +0100

    Revert "rechercher encore et toujours"
    This reverts commit 079d385ffdf524fd042e38378efbd54ea885414b.

--- src/servlet/ResultSearchServlet.java
+++ src/servlet/ResultSearchServlet.java
@@ -17,7 +17,6 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.labs.repackaged.org.json.JSONArray;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
@@ -40,50 +39,63 @@ public class ResultSearchServlet extends HttpServlet {
 		
 }
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		JSONArray arrayJson = new JSONArray();
-		String research =request.getParameter("search");
-		//creation d'un filtre permetant de reperer le nom de notre training plan
-		Filter NameFilter =
-				  new FilterPredicate("inputTitle",
-				                      FilterOperator.EQUAL,
-				                      research);
-
-		
+	
+		String research =request.getParameter("research");
 		// Utilisation Query afin de rassembler les éléments a appeler/filter 
-				Query qTraining = new Query("Trainning").setFilter(NameFilter);
-				//Récupération du résultat de la requète à l’aide de PreparedQuery 
-				PreparedQuery pq = datastore.prepare(qTraining);
+		Query qTraining = new Query("Trainning");
+		//Récupération du résultat de la requète à l’aide de PreparedQuery 
+		PreparedQuery pq = datastore.prepare(qTraining);
 		
-		for (Entity result : pq.asIterable()) 
-		{// parcours les lignes des resultats notre filtre 
+		String TrainingPlansInfos ="";
+		String ExerciceInfos="";
+		
+		for (Entity Trainingresult : pq.asIterable()) {
+			String TitleTraining = (String)Trainingresult.getProperty("inputTitle");
+			//mise en place des filtres
+			Filter Titlefilter = new FilterPredicate ("InputTitle", Query.FilterOperator.EQUAL, TitleTraining);
 			
-			  JSONObject json = new JSONObject();
-			 
-			  
-			  
-			  String inputTitle =(String) result.getProperty("inputTitle");
-			  String timeID =(String) result.getProperty("timeID");
-			  
-			  
-			  
-			  try 
-			  {
-				json.put("inputTitle",inputTitle);
-				json.put("timeID",timeID);
-				arrayJson.put(json);
-			  }
-			  catch (JSONException e) 
-			  {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			  }
-				response.setContentType("application/json");
-				PrintWriter out = response.getWriter();
-
-				out.print(arrayJson);
-				out.flush();
-
+			if(TitleTraining.contains(research)){
+				int dureeTraining=0;
+				Query qExerciceTime = new Query("Exercice");
+				qExerciceTime.setFilter(Titlefilter);
+				PreparedQuery pqExerciceTime = datastore.prepare(qExerciceTime);
+				for (Entity resultExerciceTime : pqExerciceTime.asIterable()) {
+					String dureeExercice = (String)resultExerciceTime.getProperty("exercicesTime");
+					
+					dureeExercice= dureeExercice.substring(0, dureeExercice.length()-3);
+					dureeTraining += Integer.parseInt(dureeExercice);
+				}
+				String dureeTrainingS = dureeTraining + "min";
+				
+				TrainingPlansInfos += "nextvalue" + TitleTraining + "nextvalue" + dureeTrainingS + "nexttraining";
+				
 			}
 		}
+		Query qExercice = new Query("Exercice");
+		PreparedQuery pqExercice = datastore.prepare(qExercice);
+		for (Entity resultExercice : pqExercice.asIterable()) 
+		{
+			String TitleExercice = (String)resultExercice.getProperty("exercicetitle");
+			String dureeExercice = (String)resultExercice.getProperty("exercicesTime");
+			
+			if(TitleExercice.contains(research)){
+				ExerciceInfos += "nextvalue" + TitleExercice + "nextvalue" + dureeExercice + "nexttraining";
+			}
+			
+		}
+		//Format the answer
+		JSONObject json= new JSONObject();
+		try {
+			json.put("training", TrainingPlansInfos);
+			json.put("exercice", ExerciceInfos);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+
+		out.print(json);
+		out.flush();
 	}
+}
