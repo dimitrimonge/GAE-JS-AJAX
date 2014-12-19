079d385ffdf524fd042e38378efbd54ea885414b
dimitritrimonge <mongedimitri@gmail.com>
2014-12-19 17:43:03 +0100

    rechercher encore et toujours

--- war/js/result.js
+++ war/js/result.js
@@ -1,17 +1,24 @@
-$(document).ready(function(){
+function recupResult()
+{
+	//var titre= getParameterByName('titre');
+		var search = location.search.split('search=')[1];	// recupere le titre du training plan associé
+	/*	$.ajax({
+			type:"POST",
+			dataType:'json',
+			url:'resultsearch'})*/
+		$.post('resultsearch',{'search':search},function(responsesearch){
+			
+			alert(responsesearch);
+			var obj = JSON.parse(responseText);
+			//alert obj;
+			var nom=obj.arrayObj[0].title + " " + obj.arrayObj[0].descr + " " + obj.arrayObj[0].time ; 
+			
+			var nom2=obj.arrayObj[1].title + " " + obj.arrayObj[1].descr + " " + obj.arrayObj[1].time ; 
+			alert(nom);
+			alert(nom2);
+		});
+}
 
-	
-	  	$.ajax({
-	  		type:"POST",
-	  		dataType:'json',
-	  		url:'resultsearch'
-	  	}).then(function(data){
-	  		$('#exercice').text(data.exercice);
-	  		$('#training').text(data.training);
-
-	  		
-	  	});
-});
 
 function redirectToGoogle(){
   	$.ajax({
